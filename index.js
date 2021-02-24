880cdafba7fcb80a56e41dedb88cb3ca97cc522a
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-02-24 16:01:36 +0000

    Wrote prompts for inquirer

--- index.js
+++ index.js
@@ -21,16 +21,76 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFileSync(fileName, generateMarkdown(data), (err) =>
+    fs.writeFileSync(fileName, generateMarkdown(data), (err) => {
         if (err) {
             return console.log(err); 
         }
         console.log('Your file has been successfully written to README.md');
-        });
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+           type: 'input',
+            name: 'title',
+            message: questions[0], 
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[1], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[2], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[3], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[4], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[5], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[6], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[7], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[8], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[9], 
+         },
+         {
+            type: 'input',
+            name: 'title',
+            message: questions[10], 
+         },
+    ]).then((data) =>
+    writeToFile("ReadMe.md", data)
+    )
+}
 
 // Function call to initialize app
 init();
