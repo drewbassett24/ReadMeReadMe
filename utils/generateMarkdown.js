fb166302d32c98c83a3008bf05f173e844035e51
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-02-26 12:21:20 +0000

    Finished the render license link function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,11 +22,11 @@ function renderLicenseLink(license) {
   if (license) {
     switch (license) {
       case 'MIT License':
-        return '\n\n> This project was [!MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://1besson.mit-license.org/)';
+        return '\n\n> This project was created under the standard MIT license. \n\n[Learn more about this license.](https://lbesson.mit-license.org/)';
       case 'Apache 2.0 License':
-        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        return '\n\n> This project was created under the Apache License, Version 2.0\n\n[Learn more about this license.](https://www.apache.org/licenses/LICENSE-2.0)';
       case 'GPLv3 License':
-        return '[!GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)';
+        return '\n\n> This project was created under the GNU General Public License.\n\n[Learn more about this license.]](http://lbesson.mit-license.org/)';
       default: "";
         break;
 
